9e295f256a50d6abf137ee7d26fe42b10c6ba379
GabrielTalledo <gabriel_alejandro.tm@hotmail.com>
2024-06-13 19:14:19 -0500

    FEAT: START OF LAB

---
+++ src/test/java/com/example/lab8_20211755/Lab820211755ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lab8_20211755;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab820211755ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
